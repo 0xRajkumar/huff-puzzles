c24abd9de2fff9a61d144f6f644ab74c7e9f1afe
Rajkumar <rajkumarcoolx@gmail.com>
2024-02-19 14:29:03 +0530

    Donations

--- test/Donations.t.sol
+++ test/Donations.t.sol
@@ -18,12 +18,16 @@ contract DonationsTest is Test {
 
     function testDonations(uint256[] memory amounts) public {
         uint256 sum;
-        for (uint256 i; i < amounts.length;) {
+        for (uint256 i; i < amounts.length; ) {
             vm.deal(address(this), amounts[i]);
             sum += amounts[i];
-            (bool success,) = address(donations).call{value: amounts[i]}("");
+            (bool success, ) = address(donations).call{value: amounts[i]}("");
             require(success, "call failed");
-            assertEq(donations.donated(address(this)), sum, "Wrong expected donated balance");
+            assertEq(
+                donations.donated(address(this)),
+                sum,
+                "Wrong expected donated balance"
+            );
             unchecked {
                 if (amounts.length == ++i || sum + amounts[i] < sum) break;
             }
